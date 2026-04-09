25412ec41bb80960b4c9db72158e2d472d8e7a73
Be139 <1584144681qq@gmail.com>
2026-04-09 17:02:44 +0800

    更新A

---
+++ Assets/ColorCubeController.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ColorCubeController : MonoBehaviour,Iinteractable
+{
+    public string GetDescription()
+    {
+        return "Change Color";
+    }
+
+    public void Interact()
+    {
+        
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
